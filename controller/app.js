869b7dd77fd07d5e16647b4fef53dc52e8ad2b82
Allan Morales Prado <allan.moralesp@utem.cl>
2023-07-27 03:06:11 -0400

    levantando servidor

---
+++ controller/app.js
@@ -0,0 +1,9 @@
+const getAppHealthCheckerController = (req, res, next) => {
+    res.status(200).json({
+        message: 'API health is good to go...',
+    });
+};
+
+module.exports = {
+    getAppHealthCheckerController,
+};
